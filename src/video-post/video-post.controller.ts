897e7b3613ea06f637a1e8dcf204897898cd2fff
Hoan Le <ledaihoan@gmail.com>
2024-07-12 02:27:53 +0700

    feat: refine video post create endpoint

--- src/video-post/video-post.controller.ts
+++ src/video-post/video-post.controller.ts
@@ -5,14 +5,18 @@ import { CreateVideoPostDto } from './dtos';
 import { Public } from '../utils/public.decorator';
 import { SearchVideoPostDto } from './dtos/search-video-post.dto';
 import { CursorPaginationQuery } from '../utils/cursor-pagination-query';
+import { ClientData, UserData } from '../utils/user-data.decorator';
 
 @Controller('video-posts')
 export class VideoPostController {
   constructor(private readonly service: VideoPostService) {}
 
   @Post()
-  async create(@Body() dto: CreateVideoPostDto) {
-    return this.service.createVideoPost(dto);
+  async create(
+    @Body() dto: CreateVideoPostDto,
+    @ClientData() clientData: UserData,
+  ) {
+    return this.service.createVideoPost({ ...dto, userId: clientData.id });
   }
 
   @Public()
